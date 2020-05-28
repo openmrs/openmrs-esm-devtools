3232d50f1aa75da94d2220dd5b4575b8df9a9521
Florian Rappl <florian.rappl@smapiot.com>
2020-05-28 11:45:17 +0200

    Refined approach for activation function

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import "./set-public-path";
 function setupOpenMRS() {
   return {
     lifecycle: () => import("./openmrs-esm-devtools"),
-    activate: () => !localStorage.getItem("openmrs:devtools"),
+    activate: () => !!localStorage.getItem("openmrs:devtools"),
   };
 }
 
