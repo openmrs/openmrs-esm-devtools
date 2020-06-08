8c0349f31ee3de8b368aa5a2146b4524f91d6884
Florian Rappl <rappl.florian@gmail.com>
2020-06-08 09:13:36 +0200

    Decentralized Activation (#24)
    * Prepared for distributed activation
    
    * Refined approach for activation function
    
    * Updated travis definition

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import "./set-public-path";
+
+function setupOpenMRS() {
+  return {
+    lifecycle: () => import("./openmrs-esm-devtools"),
+    activate: () => !!localStorage.getItem("openmrs:devtools"),
+  };
+}
+
+export { setupOpenMRS };
--- src/openmrs-esm-devtools.tsx
+++ src/openmrs-esm-devtools.tsx
@@ -1,16 +1,13 @@
-import "./set-public-path";
 import "./openmrs-backend-dependencies";
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./root.component";
 import singleSpaReact from "single-spa-react";
 
-const reactLifecycles = singleSpaReact({
+const { bootstrap, mount, unmount } = singleSpaReact({
   React,
   ReactDOM,
   rootComponent: Root,
 });
 
-export const bootstrap = reactLifecycles.bootstrap;
-export const mount = reactLifecycles.mount;
-export const unmount = reactLifecycles.unmount;
+export { bootstrap, mount, unmount };
