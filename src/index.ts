c10eb6a8daa600a78a0d9bd621c42dcc09f4a858
Florian Rappl <rappl.florian@gmail.com>
2020-10-13 18:05:59 +0200

    New App Shell Tooling (#34)
    * Consolidated and aligned repo structure
    
    * Prettified
    
    * Updated TypeScript
    
    * Updated root decorator
    
    * Finished adoption
    
    * Final touches

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import "./set-public-path";
+declare global {
+  interface Window {
+    spaEnv: "production" | "development" | "test";
+  }
+}
 
 function setupOpenMRS() {
   return {
     lifecycle: () => import("./openmrs-esm-devtools"),
-    activate: () => !!localStorage.getItem("openmrs:devtools"),
+    activate: () =>
+      window.spaEnv === "development" ||
+      !!localStorage.getItem("openmrs:devtools"),
   };
 }
 
